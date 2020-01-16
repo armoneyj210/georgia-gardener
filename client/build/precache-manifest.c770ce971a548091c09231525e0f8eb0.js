012ae8b63a720b31a4e1795d6fb9e7fb55790649
Joshua Armoney <armoneyj210@gmail.com>
2020-01-16 09:19:16 -0500

    initial

---
+++ client/build/precache-manifest.c770ce971a548091c09231525e0f8eb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281e3a1aa06ca1bc1e0287e9958adc1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "306e1db976f4795cb71c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "306e1db976f4795cb71c",
+    "url": "/static/js/main.bd377abe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
