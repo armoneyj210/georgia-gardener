78cb3fad1067c0e176a67064f42383f4200a9898
Joshua Armoney <armoneyj210@gmail.com>
2020-01-17 13:31:12 -0500

    plant.jsx form

--- client/src/components/Plants.jsx
+++ client/src/components/Plants.jsx
@@ -44,7 +44,49 @@ export default class Plants extends Component {
     return (
       <div>
         <h1>Plants</h1>
-        <div></div>
+        <div>
+          <button>Add New Plant</button>
+        </div>
+        <br />
+        {this.state.plantForm ? (
+          <form>
+            <div>
+              <label htmlFor="plant-name">Name:</label>
+              <input type="text" name="name" value={this.state.newPlant.name} />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="plant-description">Description:</label>
+              <input
+                type="text"
+                name="description"
+                value={this.state.newPlant.description}
+              />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="plant-image">Image:</label>
+              <input
+                type="text"
+                name="image"
+                value={this.state.newPlant.image}
+              />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="plant-disease">Common Disease:</label>
+              <input
+                type="text"
+                name="commonDisease"
+                value={this.state.newPlant.commonDisease}
+              />
+            </div>
+            <br />
+            <input type="submit" value="Create Plant" />
+          </form>
+        ) : (
+          plants
+        )}
       </div>
     );
   }
