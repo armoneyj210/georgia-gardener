6fa0ed1d1ccb97be55a3513ca0e840dbe4f004df
Joshua Armoney <armoneyj210@gmail.com>
2020-01-17 13:10:39 -0500

    plant.jsx comp di mount

--- client/src/components/Plants.jsx
+++ client/src/components/Plants.jsx
@@ -11,7 +11,15 @@ export default class Plants extends Component {
     },
     plantForm: false
   };
-
+  updatePage = () => {
+    axios.get("/api/disease").then(res => {
+      this.setState({ disease: res.data });
+      console.log(this.state.disease);
+    });
+  };
+  componentDidMount() {
+    this.updatePage();
+  }
   render() {
     return <div></div>;
   }
