d4461e6ded5023c4e24a06940a7c9e82a967a5f4
Joshua Armoney <armoneyj210@gmail.com>
2020-01-17 15:15:37 -0500

    singlePLant jsx start

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SinglePlant from "./components/SinglePlant";
 import Diseases from "./components/Diseases";
 import Plants from "./components/Plants";
 import Chemical from "./components/Chemical";
@@ -13,6 +14,7 @@ export default class App extends Component {
             <Route exact path="/chemical" component={Chemical} />
             <Route exact path="/disease" component={Diseases} />
             <Route exact path="/plant" component={Plants} />
+            <Route exact path="/plant/plantId" component={SinglePlant} />
           </Switch>
         </Router>
       </div>
---
+++ client/src/components/SinglePlant.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import axios from "axios";
+export default class SinglePlant extends Component {
+  state = {
+    plants: {
+      name: "",
+      image: "",
+      description: ""
+    }
+  };
+  render() {
+    return <div></div>;
+  }
+}
